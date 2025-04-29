7e30006330a152ed0da255c75ed8485ffedb2ed2
Daniel Kiluange Assiz <daniel.kiluange@gmail.com>
2025-04-29 13:41:41 -0300

    tests: change for 0.1.0

---
+++ test/basic_test.go
@@ -0,0 +1,40 @@
+package test
+
+import (
+	. "github.com/party-u/structure-validator"
+	"testing"
+	"time"
+)
+
+func TestBasicValidator(t *testing.T) {
+	maxLengthRule := Rule[string]{
+		Validate: func(v string) *RuleError {
+			if len(v) > 8 {
+				return &RuleError{
+					Message:    "too big max length",
+					IsCritical: true,
+					Cause:      nil,
+					Errors:     nil,
+				}
+			}
+			return nil
+		},
+		Metadata: RuleMetadata{
+			Name: "max_length_string",
+		},
+	}
+
+	conf := EngineConfiguration(
+		WithMaxRules(10),
+		WithTimeout(time.Second),
+	)
+
+	anz := NewAnalyzer(conf, maxLengthRule)
+
+	result := NewRuleValidator(anz).Analyze("aaaaaaaaa")
+
+	if len(result) != 1 {
+		t.Errorf("expect result length 1, got %d", len(result))
+	}
+
+}
