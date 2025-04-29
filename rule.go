b8e60309e4e68254d3612fa31e2d68ac19de441b
Daniel Kiluange Assiz <daniel.kiluange@gmail.com>
2025-04-29 13:53:09 -0300

    doc: change for 0.1.0

--- rule.go
+++ rule.go
@@ -1,5 +1,6 @@
 package structure_validator
 
+// Rule represents a validation rule that processes an input of type T and returns a RuleError if the rule is violated.
 type Rule[T any] struct {
 	Validate func(T) *RuleError
 	Priority int
